c35ae8940d74540cd580805cb70db808af24f596
Tomas <dy70599@gmail.com>
2017-12-03 02:11:08 +0800

    bootstrap style pages created

--- db_demo_1/views.py
+++ db_demo_1/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-#
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from exts import db
 from models import User
 import config
@@ -13,6 +13,14 @@ db.init_app(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    else:
+        pass
 
 
 if __name__ == "__main__":
